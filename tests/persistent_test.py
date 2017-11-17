f4bd26a5c3484dbd8b6f7e672e82bdb69ec1195c
c0fec0de <c0fec0de@gmail.com>
2017-11-17 22:40:41 +0100

    py2.x compatiblity

--- tests/persistent_test.py
+++ tests/persistent_test.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from tempfile import TemporaryDirectory
+from tempfile import mkdtemp
 
 from nose.tools import eq_
 
@@ -8,41 +8,40 @@ from anycache import AnyCache
 
 def test_persistent():
     """Persistent Cache over multiple instances."""
-    with TemporaryDirectory() as cachedir:
-        ac = AnyCache(cachedir=cachedir)
+    cachedir = Path(mkdtemp())
+    ac = AnyCache(cachedir=cachedir)
 
-        @ac.decorate()
-        def myfunc(posarg, kwarg=3):
-            # count the number of calls
-            myfunc.callcount += 1
-            return posarg + kwarg
-        myfunc.callcount = 0
+    @ac.decorate()
+    def myfunc(posarg, kwarg=3):
+        # count the number of calls
+        myfunc.callcount += 1
+        return posarg + kwarg
+    myfunc.callcount = 0
 
-        eq_(myfunc(4, 5), 9)
-        eq_(myfunc.callcount, 1)
-        eq_(myfunc(4, 5), 9)
-        eq_(myfunc.callcount, 1)
-        eq_(myfunc(4, 2), 6)
-        eq_(myfunc.callcount, 2)
+    eq_(myfunc(4, 5), 9)
+    eq_(myfunc.callcount, 1)
+    eq_(myfunc(4, 5), 9)
+    eq_(myfunc.callcount, 1)
+    eq_(myfunc(4, 2), 6)
+    eq_(myfunc.callcount, 2)
 
-        del ac
+    del ac
+    eq_(len(tuple(Path(cachedir).glob("*.cache"))), 2)
 
-        ac = AnyCache(cachedir=cachedir)
+    ac = AnyCache(cachedir=cachedir)
 
-        @ac.decorate()
-        def myfunc(posarg, kwarg=3):
-            # count the number of calls
-            myfunc.callcount += 1
-            return posarg + kwarg
-        myfunc.callcount = 0
+    @ac.decorate()
+    def myfunc(posarg, kwarg=3):
+        # count the number of calls
+        myfunc.callcount += 1
+        return posarg + kwarg
+    myfunc.callcount = 0
 
-        eq_(myfunc(4, 5), 9)
-        eq_(myfunc.callcount, 0)
-        eq_(myfunc(4, 5), 9)
-        eq_(myfunc.callcount, 0)
-        eq_(myfunc(4, 2), 6)
-        eq_(myfunc.callcount, 0)
+    eq_(myfunc(4, 5), 9)
+    eq_(myfunc.callcount, 0)
+    eq_(myfunc(4, 5), 9)
+    eq_(myfunc.callcount, 0)
+    eq_(myfunc(4, 2), 6)
+    eq_(myfunc.callcount, 0)
 
-        del ac
-
-        eq_(len(tuple(Path(cachedir).glob("*.cache"))), 2)
+    ac.clear()
